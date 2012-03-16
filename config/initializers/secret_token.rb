18c5d5ac47c8a58733e3e7c1f2b08b7f354e2745
Nithya Rajaram <nithya@supportbee.com>
2012-03-16 11:54:13 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Occsantiago::Application.config.secret_token = '05dae8f1842d89398acb740d46b2207808c0868eef97739c547a3269a145aaba569d6df65bacaae437d49af52aeaf2f7d51ea697f5549e2a9b2330f21ae05db8'
